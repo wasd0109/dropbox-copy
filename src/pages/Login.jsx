245e5f8a4bfc20ad8d87d5ba7547710e3e29ecf5
Ken Cheung <wasd0109.dev@gmail.com>
2021-08-11 20:07:01 +0900

    Environment set

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+
+function Login() {
+  return <Link to="/register">To register</Link>;
+}
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Register() {
+  return <Link to="/login">To login</Link>;
+}
+
+export default Register;
