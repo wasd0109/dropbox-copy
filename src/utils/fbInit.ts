c7de3d86af7be0c415a0e93015bda2c72c24485b
Ken Cheung <wasd0109.dev@gmail.com>
2021-08-14 16:37:24 +0900

    Change useEmulator to when dev only

--- src/utils/fbInit.ts
+++ src/utils/fbInit.ts
@@ -4,11 +4,14 @@ import firebaseConfig from "./fbConfig";
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-auth.useEmulator("http://localhost:9099");
 const db = firebase.firestore();
-db.useEmulator("localhost", 8080);
 
 const storage = firebase.storage();
-storage.useEmulator("localhost", 9199);
+
+if (process.env.NODE_ENV === "development") {
+  auth.useEmulator("http://localhost:9099");
+  db.useEmulator("localhost", 8080);
+  storage.useEmulator("localhost", 9199);
+}
 
 export { auth, db, storage };
