934961659337730ef6fdb02c344ab11c429c0b4b
Ken Cheung <wasd0109.dev@gmail.com>
2021-08-14 16:51:02 +0900

    Change github action cache setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register/Register.tsx";
+import Register from "./pages/Register";
 
 function App() {
   return (
